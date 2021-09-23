3a33f6a1fd52af317f064380ee7a2bd9863bd687
danielprudent <52863416+danielprudent@users.noreply.github.com>
2021-09-23 15:41:43 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,299 @@
+
+
+	var myArray = [
+
+    {
+        "id": 1,
+        "title": "Beetlejuice",
+        "year": "1988",
+        "runtime": "92 min",
+        "genres": [
+            "Comedy",
+            "Fantasy"
+        ],
+        "director": "Tim Burton",
+        "actors": "Alec Baldwin, Geena Davis, Annie McEnroe, Maurice Page",
+        "plot": "A couple of recently deceased ghosts contract the services of a \"bio-exorcist\" in order to remove the obnoxious new owners of their house.",
+        "posterUrl": "https://www.wonderwall.com/photos/2016/10/18/104-192111_Actual.jpg?x=490&y=800&icq=74&sig=ee335ae84bd459c1ffe021ca839748be"
+    },
+    {
+        "id": 2,
+        "title": "The Cotton Club",
+        "year": "1984",
+        "runtime": "127 min",
+        "genres": [
+            "Crime",
+            "Drama",
+            "Music"
+        ],
+        "director": "Francis Ford Coppola",
+        "actors": "Richard Gere, Gregory Hines, Diane Lane, Lonette McKee",
+        "plot": "The Cotton Club was a famous night club in Harlem. The story follows the people that visited the club, those that ran it, and is peppered with the Jazz music that made it so famous.",
+        "posterUrl": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTU5ODAyNzA4OV5BMl5BanBnXkFtZTcwNzYwNTIzNA@@._V1_SX300.jpg"
+    },
+    {
+        "id": 3,
+        "title": "The Shawshank Redemption",
+        "year": "1994",
+        "runtime": "142 min",
+        "genres": [
+            "Crime",
+            "Drama"
+        ],
+        "director": "Frank Darabont",
+        "actors": "Tim Robbins, Morgan Freeman, Bob Gunton, William Sadler",
+        "plot": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+        "posterUrl": "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg"
+      },
+      {
+          "id": 4,
+          "title": "Crocodile Dundee",
+          "year": "1986",
+          "runtime": "97 min",
+          "genres": [
+              "Adventure",
+              "Comedy"
+          ],
+          "director": "Peter Faiman",
+          "actors": "Paul Hogan, Linda Kozlowski, John Meillon, David Gulpilil",
+          "plot": "An American reporter goes to the Australian outback to meet an eccentric crocodile poacher and invites him to New York City.",
+          "posterUrl": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTg0MTU1MTg4NF5BMl5BanBnXkFtZTgwMDgzNzYxMTE@._V1_SX300.jpg"
+      },
+      {
+          "id": 5,
+          "title": "Valkyrie",
+          "year": "2008",
+          "runtime": "121 min",
+          "genres": [
+              "Drama",
+              "History",
+              "Thriller"
+          ],
+          "director": "Bryan Singer",
+          "actors": "Tom Cruise, Kenneth Branagh, Bill Nighy, Tom Wilkinson",
+          "plot": "A dramatization of the 20 July assassination and political coup plot by desperate renegade German Army officers against Hitler during World War II.",
+          "posterUrl": "https://ia.media-imdb.com/images/M/MV5BMTg3Njc2ODEyN15BMl5BanBnXkFtZTcwNTAwMzc3NA@@._V1_SX300.jpg"
+      },
+      {
+          "id": 6,
+          "title": "Ratatouille",
+          "year": "2007",
+          "runtime": "111 min",
+          "genres": [
+              "Animation",
+              "Comedy",
+              "Family"
+          ],
+          "director": "Brad Bird, Jan Pinkava",
+          "actors": "Patton Oswalt, Ian Holm, Lou Romano, Brian Dennehy",
+          "plot": "A rat who can cook makes an unusual alliance with a young kitchen worker at a famous restaurant.",
+          "posterUrl": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMzODU0NTkxMF5BMl5BanBnXkFtZTcwMjQ4MzMzMw@@._V1_SX300.jpg"
+      },
+      {
+          "id": 7,
+          "title": "City of God",
+          "year": "2002",
+          "runtime": "130 min",
+          "genres": [
+              "Crime",
+              "Drama"
+          ],
+          "director": "Fernando Meirelles, Kátia Lund",
+          "actors": "Alexandre Rodrigues, Leandro Firmino, Phellipe Haagensen, Douglas Silva",
+          "plot": "Two boys growing up in a violent neighborhood of Rio de Janeiro take different paths: one becomes a photographer, the other a drug dealer.",
+          "posterUrl": "https://images-na.ssl-images-amazon.com/images/M/MV5BMjA4ODQ3ODkzNV5BMl5BanBnXkFtZTYwOTc4NDI3._V1_SX300.jpg"
+      },
+      {
+          "id": 8,
+          "title": "Memento",
+          "year": "2000",
+          "runtime": "113 min",
+          "genres": [
+              "Mystery",
+              "Thriller"
+          ],
+          "director": "Christopher Nolan",
+          "actors": "Guy Pearce, Carrie-Anne Moss, Joe Pantoliano, Mark Boone Junior",
+          "plot": "A man juggles searching for his wife's murderer and keeping his short-term memory loss from being an obstacle.",
+          "posterUrl": "https://render.fineartamerica.com/images/rendered/default/poster/8/10/break/images/artworkimages/medium/1/memento-poster-david-djanbaz.jpg"
+      },
+      {
+          "id": 9,
+          "title": "The Intouchables",
+          "year": "2011",
+          "runtime": "112 min",
+          "genres": [
+              "Biography",
+              "Comedy",
+              "Drama"
+          ],
+          "director": "Olivier Nakache, Eric Toledano",
+          "actors": "François Cluzet, Omar Sy, Anne Le Ny, Audrey Fleurot",
+          "plot": "After he becomes a quadriplegic from a paragliding accident, an aristocrat hires a young man from the projects to be his caregiver.",
+          "posterUrl": "https://ia.media-imdb.com/images/M/MV5BMTYxNDA3MDQwNl5BMl5BanBnXkFtZTcwNTU4Mzc1Nw@@._V1_SX300.jpg"
+      },
+      {
+          "id": 10,
+          "title": "Stardust",
+          "year": "2007",
+          "runtime": "127 min",
+          "genres": [
+              "Adventure",
+              "Family",
+              "Fantasy"
+          ],
+          "director": "Matthew Vaughn",
+          "actors": "Ian McKellen, Bimbo Hart, Alastair MacIntosh, David Kelly",
+          "plot": "In a countryside town bordering on a magical land, a young man makes a promise to his beloved that he'll retrieve a fallen star by venturing into the magical realm.",
+          "posterUrl": "https://images-na.ssl-images-amazon.com/images/M/MV5BMjkyMTE1OTYwNF5BMl5BanBnXkFtZTcwMDIxODYzMw@@._V1_SX300.jpg"
+      },
+      {
+          "id": 11,
+          "title": "Apocalypto",
+          "year": "2006",
+          "runtime": "139 min",
+          "genres": [
+              "Action",
+              "Adventure",
+              "Drama"
+          ],
+          "director": "Mel Gibson",
+          "actors": "Rudy Youngblood, Dalia Hernández, Jonathan Brewer, Morris Birdyellowhead",
+          "plot": "As the Mayan kingdom faces its decline, the rulers insist the key to prosperity is to build more temples and offer human sacrifices. Jaguar Paw, a young man captured for sacrifice, flees to avoid his fate.",
+          "posterUrl": "https://images-na.ssl-images-amazon.com/images/M/MV5BNTM1NjYyNTY5OV5BMl5BanBnXkFtZTcwMjgwNTMzMQ@@._V1_SX300.jpg"
+      },
+      {
+          "id": 12,
+          "title": "Taxi Driver",
+          "year": "1976",
+          "runtime": "113 min",
+          "genres": [
+              "Crime",
+              "Drama"
+          ],
+          "director": "Martin Scorsese",
+          "actors": "Diahnne Abbott, Frank Adu, Victor Argo, Gino Ardito",
+          "plot": "A mentally unstable Vietnam War veteran works as a night-time taxi driver in New York City where the perceived decadence and sleaze feeds his urge for violent action, attempting to save a preadolescent prostitute in the process.",
+          "posterUrl": "https://m.media-amazon.com/images/M/MV5BM2M1MmVhNDgtNmI0YS00ZDNmLTkyNjctNTJiYTQ2N2NmYzc2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
+      },
+      {
+          "id": 13,
+          "title": "No Country for Old Men",
+          "year": "2007",
+          "runtime": "122 min",
+          "genres": [
+              "Crime",
+              "Drama",
+              "Thriller"
+          ],
+          "director": "Ethan Coen, Joel Coen",
+          "actors": "Tommy Lee Jones, Javier Bardem, Josh Brolin, Woody Harrelson",
+          "plot": "Violence and mayhem ensue after a hunter stumbles upon a drug deal gone wrong and more than two million dollars in cash near the Rio Grande.",
+          "posterUrl": "https://images-na.ssl-images-amazon.com/images/M/MV5BMjA5Njk3MjM4OV5BMl5BanBnXkFtZTcwMTc5MTE1MQ@@._V1_SX300.jpg"
+      },
+      {
+          "id": 14,
+          "title": "Planet 51",
+          "year": "2009",
+          "runtime": "91 min",
+          "genres": [
+              "Animation",
+              "Adventure",
+              "Comedy"
+          ],
+          "director": "Jorge Blanco, Javier Abad, Marcos Martínez",
+          "actors": "Jessica Biel, John Cleese, Gary Oldman, Dwayne Johnson",
+          "plot": "An alien civilization is invaded by Astronaut Chuck Baker, who believes that the planet was uninhabited. Wanted by the military, Baker must get back to his ship before it goes into orbit without him.",
+          "posterUrl": "https://ia.media-imdb.com/images/M/MV5BMTUyOTAyNTA5Ml5BMl5BanBnXkFtZTcwODU2OTM0Mg@@._V1_SX300.jpg"
+      },
+      {
+          "id": 15,
+          "title": "Looper",
+          "year": "2012",
+          "runtime": "119 min",
+          "genres": [
+              "Action",
+              "Crime",
+              "Drama"
+          ],
+          "director": "Rian Johnson",
+          "actors": "Joseph Gordon-Levitt, Bruce Willis, Emily Blunt, Paul Dano",
+          "plot": "In 2074, when the mob wants to get rid of someone, the target is sent into the past, where a hired gun awaits - someone like Joe - who one day learns the mob wants to 'close the loop' by sending back Joe's future self for assassination.",
+          "posterUrl": "https://ia.media-imdb.com/images/M/MV5BMTY3NTY0MjEwNV5BMl5BanBnXkFtZTcwNTE3NDA1OA@@._V1_SX300.jpg"
+      }
+
+
+	]
+
+    $('#search-input').on('keyup',function(){
+        var value = $(this).val()
+        console.log('Value:', value)
+        var data = searchTable(value, myArray)
+        buildTable(data)
+    })
+
+
+
+	buildTable(myArray)
+
+    function searchTable(value, data){
+        var filteredData = []
+
+        for (var i = 0; i < data.length; i++){
+            value = value.toLowerCase()
+            var name = data[i].name.toLowerCase()
+
+             if (name.includes(value)){
+                 filteredData.push(data[i])
+
+             }
+        }
+
+        return filteredData
+    }
+
+  $('th').on('click', function(){
+      var column = $(this).data('colname')
+      var order = $(this).data('order')
+      var text = $(this).html()
+      text = text.substring(0, text.length - 1);
+
+
+
+      if (order == 'desc'){
+         myArray = myArray.sort((a, b) => a[column] > b[column] ? 1 : -1)
+         $(this).data("order","asc");
+         text += '&#9660'
+      }else{
+         myArray = myArray.sort((a, b) => a[column] < b[column] ? 1 : -1)
+         $(this).data("order","desc");
+         text += '&#9650'
+      }
+
+     $(this).html(text)
+     buildTable(myArray)
+     })
+
+	function buildTable(data){
+		var table = document.getElementById('myTable')
+
+        table.innerHTML = ''
+
+		for (var i = 0; i < data.length; i++){
+			var row = `<tr>
+
+							<td>${data[i].title}</td>
+							<td>${data[i].year}</td>
+							<td>${data[i].runtime}</td>
+              <td>${data[i].genres}</td>
+              <td>${data[i].director}</td>
+              <td>${data[i].actors}</td>
+              <td>${data[i].plot}</td>
+              <img class="posterUrl" src="${data[i].posterUrl}""></img>
+					  </tr>`
+			table.innerHTML += row
+
+
+
+		}
+	}
